be3e8424ca391410474eac666f12362a28237bbb
yousef fathy <yf1102003@gmail.com>
2023-04-27 01:53:11 +0200

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/R. Age in Days.cpp
+++ ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/R. Age in Days.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
- 
- 
+
 int main(){
-    
+    int age ;
+    cin>>age;
+    cout<<(age/365)<<" years"<<endl;
+    cout<<((age%365)/30)<<" months"<<endl;
+    cout<<((age%365)%30)<<" days"<<endl;
     return 0;
 }
