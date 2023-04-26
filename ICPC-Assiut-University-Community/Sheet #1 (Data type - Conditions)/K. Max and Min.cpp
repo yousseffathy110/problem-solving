54ad5a889a051c4424eda216aef8f9f327d11999
yousef fathy <yf1102003@gmail.com>
2023-04-26 04:10:48 +0200

    Accepted

--- ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/K. Max and Min.cpp
+++ ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/K. Max and Min.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include <algorithm>
 using namespace std;
  
  
 int main(){
-    
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<min({a, b, c})<<" "<<max({a, b, c});
     return 0;
 }
