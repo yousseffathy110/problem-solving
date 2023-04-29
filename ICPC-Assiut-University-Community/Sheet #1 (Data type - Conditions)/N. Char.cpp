d7c09d60dfdc6689be5dbbb55208e9f5e38d065a
yousef fathy <yf1102003@gmail.com>
2023-04-29 17:54:51 +0300

    Accepted!!

--- ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/N. Char.cpp
+++ ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/N. Char.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
-#include <cctype>
+// #include <cctype>
 using namespace std;
  
  
 int main(){
+    // char x;
+    // cin>>x;
+    // if (isupper(x))
+    // {
+    //     cout<<static_cast<char>(tolower(x));
+    // }
+    // else if (islower(x))
+    // {
+    //     cout<<static_cast<char>(toupper(x));
+    // }
     char x;
     cin>>x;
-    if (isupper(x))
+    if (x>96)
     {
-        cout<<static_cast<char>(tolower(x));
-    }
-    else if (islower(x))
-    {
-        cout<<static_cast<char>(toupper(x));
+        cout<<char(int(x-32));
+    }else{
+        cout <<char(int(x+32));
     }
     return 0;
 }
