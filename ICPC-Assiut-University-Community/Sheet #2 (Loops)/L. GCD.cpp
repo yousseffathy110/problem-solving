6d1121e6249a1b0e1b29c97daca0063087ec6a74
yousef fathy <yf1102003@gmail.com>
2023-05-02 07:51:11 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/L. GCD.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/L. GCD.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    short x , y;
+    short x , y , reminder;
     cin >> x >> y;
+    while (y != 0)
+    {
+        reminder = x % y;
+        x = y;
+        y = reminder;
+    }    
+    cout<<x;
     return 0;
 }
---
+++ ICPC-Mansoura-community-training-for-level 0-2023/A. Watermelon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    short x; cin>>x;
+    if (x%2==0 && x!=2){cout<<"YES";}else{cout<<"NO";}
+    return 0;
+}
