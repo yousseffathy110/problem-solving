6b47ce9814f65784c050c74936fc688c4e670d09
yousef fathy <yf1102003@gmail.com>
2023-05-02 09:03:59 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/M. Lucky Numbers.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/M. Lucky Numbers.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-    
+    int x , y;
+    cin >> x >> y;
+    bool find_lucky_number = false;
+    for (int i = x; i <= y; i++)
+    {
+        bool Lucky=true;
+        int temp = i;
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            if (digit != 4 && digit != 7)
+            {
+                Lucky = false;
+                break;
+            }
+            temp /= 10;
+        }
+        if (Lucky) {
+            cout << i << " ";
+            find_lucky_number = true;
+        }
+    }
+    if (!find_lucky_number) {
+        cout << -1;
+    }
     return 0;
 }
