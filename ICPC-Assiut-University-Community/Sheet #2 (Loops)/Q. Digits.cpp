9f757ea41786f57b06d8a88972e5170d499021b7
yousef fathy <yf1102003@gmail.com>
2023-05-03 22:35:19 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/Q. Digits.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/Q. Digits.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
+    short testCases;
+    cin>>testCases;
+    for (int i = 0; i < testCases; i++)
+    {
+        string number;
+        cin>>number;
+        for (int j = number.length(); j > 0; j--)
+        {
+            cout<<number[j-1]<<" ";
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
