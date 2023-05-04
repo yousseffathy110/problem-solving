a15ed9410556a1c540e99a300336baefa822cb25
yousef fathy <yf1102003@gmail.com>
2023-05-04 16:38:53 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/V. PUM.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/V. PUM.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
+    short rows;
+    cin >> rows;
+    int x=1 , y=2 , z=3;
+    for (int i = 0; i < rows; i++)
+    {
+        cout<<x<<" "<<y<<" "<<z<<" "<< "PUM"<<endl;
+        x =x+4;
+        y=y+4;
+        z=z+4;
+    }
     
     return 0;
 }
