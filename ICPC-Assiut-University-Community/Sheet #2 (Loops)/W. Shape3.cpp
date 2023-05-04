9dac4c7288376988b13735a6b1ad583c9a9013c1
yousef fathy <yf1102003@gmail.com>
2023-05-04 18:23:57 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/W. Shape3.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/W. Shape3.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main()
 {
+    short rows;
+    cin >> rows;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= rows-i; j++)
+        {
+            cout<<" ";
+        }
+        for (int k = 1; k <= 2*i-1; k++)
+        {
+            cout <<"*";
+        }
+        cout<<endl;
+    }
+    for (int x = rows; x >= 1; x--)
+    {
+        for (int y = 0; y < rows-x; y++)
+        {
+            cout<<" ";
+        }
+        for (int z = 2*x-1; z >= 1 ; z--)
+        {
+            cout <<"*";
+        }
+        cout<<endl;
+    }
+    
     
     return 0;
 }
