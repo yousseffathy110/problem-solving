043f7c6d490b8c490b00ceca3ce667540ac0fa94
yousef fathy <yf1102003@gmail.com>
2023-05-04 16:30:18 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/U. Some Sums.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/U. Some Sums.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 int main()
 {
-    
+    int number, lowerbond, upperbond;
+    long long sum = 0;
+    cin >> number >> lowerbond >> upperbond;
+    for (int i = 1; i <= number; i++)
+    {
+        if (i / 10 == 0)
+        {
+            if (i >= lowerbond && i <= upperbond)
+            {
+                sum += i;
+            }
+            
+        }
+        else
+        {
+            int digitSum= 0;
+            int counter = i;
+            while (counter)
+            {
+                int digit = counter % 10;
+                digitSum += digit;
+                counter /= 10;
+            }
+            if (digitSum >= lowerbond && digitSum <= upperbond)
+            {
+                sum += i;
+            }
+            
+        }
+    }
+    cout << sum;
     return 0;
 }
