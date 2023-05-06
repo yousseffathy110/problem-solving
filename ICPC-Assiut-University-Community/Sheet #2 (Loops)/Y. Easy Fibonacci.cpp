b56a476df31a78b1f1b9cea488613ab61291dc27
yousef fathy <yf1102003@gmail.com>
2023-05-06 23:49:53 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/Y. Easy Fibonacci.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/Y. Easy Fibonacci.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 int main()
 {
-    short testCases;
-    cin >> testCases;
+    int a = 0, b = 1, c, Fibonacci;
+    cin >> Fibonacci;
+    if (Fibonacci == 1)
+    {
+        cout <<a;
+    }
+    else if (Fibonacci == 2)
+    {
+        cout << a <<" "<< b <<" ";
+    }
+    else
+    {
+        cout << a <<" "<< b <<" ";
+        for (int i = 2; i < Fibonacci; i++)
+        {
+            c=a+b;
+            cout <<c<<" ";
+            a=b;
+            b=c;
+        }
+    }
     return 0;
 }
