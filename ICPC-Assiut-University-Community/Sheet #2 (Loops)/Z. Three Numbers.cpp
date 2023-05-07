4c134f071986b01bbfca3e505888a2d925bb082f
yousef fathy <yf1102003@gmail.com>
2023-05-07 19:14:32 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #2 (Loops)/Z. Three Numbers.cpp
+++ ICPC-Assiut-University-Community/Sheet #2 (Loops)/Z. Three Numbers.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    
-    return 0;
+    int start, end , counter = 0;
+    cin >> start >> end;
+	for (int i = 0; i <= start; i++)
+	{
+		for (int j = 0; j <= start; j++)
+		{
+			if (end - i - j >= 0 && end - i - j <= start)
+			{
+				counter++;
+			}
+		}
+	}
+	cout << counter;
 }
