26f08227021be5ce142f7f3d81929f76b8768aa8
yousef fathy <yf1102003@gmail.com>
2023-05-08 19:20:46 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Contest#2/C. Finding Minimums.cpp
+++ ICPC-Assiut-University-Community/Contest#2/C. Finding Minimums.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
+using ll = long long;
 
 int main()
 {
-   
+	int testCases, size, counter = 0;
+	cin >> testCases >> size;
+	short index = 1;
+	ll minimum;
+	for (int i = 1; i <= testCases; i++)
+	{
+		ll numbers;
+		cin >> numbers;
+		if (index)
+		{
+			minimum = numbers;
+			index = 0;
+		}
+		else
+		{
+			if (numbers < minimum)
+			{
+				minimum = numbers;
+			}
+		}
+		counter++;
+		if (counter == size || i == testCases)
+		{
+			cout << minimum << " ";
+			counter = 0;
+			index = 1;
+		}
+	}
 }
