b410678de28bd134ae1e503f700bf7a4b711ad93
yousef fathy <yf1102003@gmail.com>
2023-05-08 20:26:59 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/E. Make a triangle!.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/E. Make a triangle!.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-    
+    short a, b, c, minutes, Maximum, minimum, mid;
+    cin >> a >> b >> c;
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        cout << "0";
+    }
+    else
+    {
+        Maximum = max({ a, b, c });
+        minimum = min({ a, b, c });
+        mid = a + b + c - Maximum - minimum;
+
+        minutes = (Maximum - mid - minimum) + 1;
+        cout << minutes;
+    }
     return 0;
 }
