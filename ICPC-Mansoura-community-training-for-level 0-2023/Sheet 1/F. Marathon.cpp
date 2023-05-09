49db19cb3ee8ca77fa003b995f68f6a9bff3018e
yousef fathy <yf1102003@gmail.com>
2023-05-09 21:15:40 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/F. Marathon.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/F. Marathon.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-
+   int  testCase, counter = 0, distance[4];
+   cin >> testCase;
+   while (testCase--)
+   {
+      for (int i = 0; i < 4; i++)
+      {
+         cin >> distance[i];
+      }
+      int timur = distance[0];
+      for (int j = 1; j < 4; j++)
+      {
+         if(timur < distance[j])
+         {
+            counter++;
+         }
+      }
+      cout << counter << endl;
+      counter = 0;
+   }
+   
    return 0;
 }
