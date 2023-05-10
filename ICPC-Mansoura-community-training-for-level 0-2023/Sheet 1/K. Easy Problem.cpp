90bb0975d8cd2064c703563d30ff2c105de788dd
yousef fathy <yf1102003@gmail.com>
2023-05-10 07:54:29 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/K. Easy Problem.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/K. Easy Problem.cpp
@@ -1,28 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     long long number;
+    cin >> number;
+    bool isPrime;
     if (number < 2)
     {
         cout << "NO";
     }
     else
     {
-        for (int i = 2; i * i <= number; i++)
+        for (int i = 2; i <= number; i++)
         {
-            if (number % i == 0)
+            isPrime = true;
+            for (int j = 2; j * j <= i; j++)
             {
-                cout << "NO";
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
             }
-            else if (number % i != 0 && number % 13 == 0)
+            if(isPrime && i % 13 == 0)
             {
                 cout << "YES";
+                return 0;
             }
-            
-            
         }
-        
+        cout << "NO";
     }
-     
+    return 0;
 }
