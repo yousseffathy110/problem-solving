2a397935d12d2f9214821dd20d7a4c8881064bb0
yousef fathy <yf1102003@gmail.com>
2023-05-10 17:48:43 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/N. Wrong Subtraction.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/N. Wrong Subtraction.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    
+    long long number;
+    short flag;
+    cin >> number >> flag;
+    while (flag > 0)
+    {
+        if (number % 10 == 0)
+        number /= 10;
+        else
+        number--;
+        flag--;
+    }
+    cout << number;
 }
