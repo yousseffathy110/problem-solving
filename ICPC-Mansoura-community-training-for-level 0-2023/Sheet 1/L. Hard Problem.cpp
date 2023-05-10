c25a04d82d7381e57abb724461f920fc887f2051
yousef fathy <yf1102003@gmail.com>
2023-05-10 08:01:48 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/L. Hard Problem.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/L. Hard Problem.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
-    
+    long long number1, number2, number3;
+    cin >> number1 >> number2 >> number3;
+    cout << min({number1, number2, number3}) << " " << ((number1 + number2 + number3) - (min({number1, number2, number3})+max({number1, number2, number3}))) << " " << max({number1, number2, number3});
     return 0;
 }
