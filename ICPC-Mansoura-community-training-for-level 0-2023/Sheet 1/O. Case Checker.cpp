b3148b332b22329128c7d8dcc478d47c948489e9
yousef fathy <yf1102003@gmail.com>
2023-05-10 17:59:33 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/O. Case Checker.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/O. Case Checker.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+    short charNumber;
+    char character;
+    cin >> charNumber;
+    while (charNumber > 0)
+    {
+        cin >> character;
+        if (islower(character))
+        cout << "is lower" << endl;
+        else if (isdigit(character))
+        cout << "is digit" << endl;
+        else
+        cout << "is upper" << endl;
+        charNumber--;
+    }
 }
