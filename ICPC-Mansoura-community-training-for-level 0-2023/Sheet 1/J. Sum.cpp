e5b351faf257c6d9d9ac5770147d2a591e533305
yousef fathy <yf1102003@gmail.com>
2023-05-10 15:54:00 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/J. Sum.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/J. Sum.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 int main()
 {
+    int testCases , a , b , c;
+    cin >> testCases;
+    while (testCases--)
+    {
+        cin >> a >> b >> c ;
+        if (a == b + c || b == a + c || c == a + b)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        
+    }
     
 }
