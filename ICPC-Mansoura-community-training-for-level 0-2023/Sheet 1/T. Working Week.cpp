ea3ef03e057b68588eeec2ada5f658a25a6069ae
yousef fathy <yf1102003@gmail.com>
2023-05-11 22:34:28 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/T. Working Week.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/T. Working Week.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
-   
+    short testCases;
+    cin >> testCases;
+    while (testCases--)
+    {
+        long long days , daysOff;
+        cin >> days;
+        
+    }
 }
