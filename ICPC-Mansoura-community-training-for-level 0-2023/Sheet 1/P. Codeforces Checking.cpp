fb24e223f9828c6e3096b2483b9df39415df2208
yousef fathy <yf1102003@gmail.com>
2023-05-11 21:56:02 +0300

    Accepted!!!

--- ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/P. Codeforces Checking.cpp
+++ ICPC-Mansoura-community-training-for-level 0-2023/Sheet 1/P. Codeforces Checking.cpp
@@ -3,5 +3,30 @@ using namespace std;
 
 int main()
 {
-    
+    short testCases;
+    char letter;
+    cin >> testCases;
+    while (testCases--)
+    {
+        cin >> letter;
+        bool found = false;
+        for (int i = 0; i < 10; i++)
+        {
+            if (letter == "codeforces"[i])
+            {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        
+    }
+    return 0;
 }
