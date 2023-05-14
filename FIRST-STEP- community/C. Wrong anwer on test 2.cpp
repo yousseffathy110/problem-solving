0dff77e11239c393ab502cb3e8c8b726b566a13b
yousef fathy <yf1102003@gmail.com>
2023-05-14 20:57:29 +0300

    Accepted!!!

--- FIRST-STEP- community/C. Wrong anwer on test 2.cpp
+++ FIRST-STEP- community/C. Wrong anwer on test 2.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long ;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+    ll number , max  = 0, min = 0;
+    cin >> number;
+    max = (number / 6) * 6;
+    cout << "The biggest is " << max << endl;
+    cout << "The smallest is " << min << endl;
 }
