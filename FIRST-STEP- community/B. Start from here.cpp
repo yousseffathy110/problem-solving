5eb22b1d293045730cc2a6e7d009b4c55267ef63
yousef fathy <yf1102003@gmail.com>
2023-05-14 21:18:56 +0300

    Accepted!!!

---
+++ FIRST-STEP- community/B. Start from here.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    double radius;
+    cin >> radius;
+    cout << fixed << setprecision(6) << 4 * radius * radius;
+}
