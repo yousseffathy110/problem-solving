e4435657d8ce910bdd8fd79092cc26bd49a040c4
yousef fathy <yf1102003@gmail.com>
2023-05-15 21:33:36 +0300

    Accepted!!!

--- FIRST-STEP- community/Contest #2/E. Minimum Moves.cpp
+++ FIRST-STEP- community/Contest #2/E. Minimum Moves.cpp
@@ -3,25 +3,21 @@
 using namespace std;
 using ll = long long;
 
-int main() {
+int main() 
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
     ll N;
     cin >> N;
-
     ll remainder = N % 6;
     ll moves = 0;
-    if (remainder == 1 || remainder == 5) {
+    if (remainder == 1 || remainder == 5) 
         moves = 1;
-    } else if (remainder == 2 || remainder == 4) {
+    else if (remainder == 2 || remainder == 4) 
         moves = 2;
-    } else if (remainder == 3) {
-        ll multiple = 6 * floor(N / 6.0);
-        moves = min(abs(N - 3 - multiple), abs(N + 3 - multiple));   
-    }
-
+    else if (remainder == 3) 
+        moves = 3;
     cout << moves << endl;
     return 0;
 }
