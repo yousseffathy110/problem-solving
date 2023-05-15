9c692359ed355b7ceab1fe6fab81638ef11984f6
yousef fathy <yf1102003@gmail.com>
2023-05-15 21:50:46 +0300

    Accepted!!!

--- FIRST-STEP- community/Contest #2/C. Who is Firsrt.cpp
+++ FIRST-STEP- community/Contest #2/C. Who is Firsrt.cpp
@@ -5,36 +5,35 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
     int r1, r2;
     cin >> r1 >> r2;
     int x1 , x2;
     cin >> x1 >> x2;
+
     if (r1 + x1  > 1600)
     {
         cout << "Mr.Sadik";
-        return 0;
     }
     else if (r2 + x2 > 1600)
     {
         cout << "Ramadan";
-        return 0;
     }
     else if (r1 + x1  > 1600 && r2 + x2 > 1600)
     {
         if ((r1 + x1) > (r2 + x2))
         {
             cout << "Mr.Sadik";
-            return 0;
         }
-        else if (((r1 + x1) > (r2 + x2)))
+        else if ((r1 + x1) < (r2 + x2))
         {
             cout << "Ramadan";
-            return 0;
         }
-        
-        
+    }
+    else if (r1 + x1 == r2 + x2)
+    {
+        cout <<"None";
     }
     else
         cout <<"None";
-        return 0;
 }
