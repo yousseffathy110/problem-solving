684f4fcf28ea854673be7084b717138b4195b49b
yousef fathy <yf1102003@gmail.com>
2023-05-16 02:10:40 +0300

    Accepted!!!

--- FIRST-STEP- community/Contest #3/B. There Exist.cpp
+++ FIRST-STEP- community/Contest #3/B. There Exist.cpp
@@ -1,44 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    long long number , prime = 0;
-    bool found_prime  = false ;
-    cin >> number;
-    if (number <= 1)
+    long long x , prime = -1;
+    cin >> x;
+    if (x <= 1)
         cout << "NO";
     else
     {
-        for (int i = 2; i * i <= number; i++)
+        for (int i = 2; i <= x; i++)
         {
-            if (number % i == 0) 
+            bool isPrime = true;
+            for (int j = 2; j <= sqrt(i); j++)
+            {        
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime && i % 7 == 0)
             {
-                bool is_prime = true;
-                for (int j = 2; j * j <= i; j++) 
-                {
-                    if (i % j == 0) 
-                    {
-                        is_prime = false;
-                        break;
-                    }
-                }
-                
-                if (is_prime) 
-                {
-                    if (i % 7 == 0) 
-                    {
-                        prime = i;
-                        found_prime = true;
-                    }
-                }
-
+                prime = i;
+                break;
             }
         }
+        if (prime != -1)
+            cout << prime;
+        else
+            cout << "NO";
     }
-    if (found_prime)
-        cout << prime;
-    else
-        cout << "NO";
 }
