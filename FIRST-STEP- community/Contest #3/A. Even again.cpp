254a736dbd5b4a4d68cdea5a3280d669c371076a
yousef fathy <yf1102003@gmail.com>
2023-05-15 21:59:07 +0300

    Accepted!!!

--- FIRST-STEP- community/Contest #3/A. Even again.cpp
+++ FIRST-STEP- community/Contest #3/A. Even again.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    
+    long long number , sum = 0;
+    cin >> number;
+    for (int i = 1; i <= number; i++)
+    {
+        if (i % 2 == 0)
+        sum += i;
+    }
+    cout << sum ;
 }
