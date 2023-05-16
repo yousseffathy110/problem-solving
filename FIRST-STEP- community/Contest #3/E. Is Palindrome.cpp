9d3fa0e3d497b4a5fa2e124f4c6f3f48b3c3b349
yousef fathy <yf1102003@gmail.com>
2023-05-16 03:19:08 +0300

    Accepted!!!

--- FIRST-STEP- community/Contest #3/E. Is Palindrome.cpp
+++ FIRST-STEP- community/Contest #3/E. Is Palindrome.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve(){
+    string s;
+    cin>>s;
+    short i=(s[0]=='-'),j=s.size()-1;
+    while(i<j)
+    {
+        if( s[i++] != s[j--] )
+        {
+            cout<<"No";
+            return;
+        }
+    }
+    cout<<"Yes";
+}
+ 
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    
+    long long testCases;
+    cin >> testCases;
+    while (testCases--)
+    {
+        solve();
+        cout << endl;
+    }
 }
