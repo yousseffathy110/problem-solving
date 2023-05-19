a386e81fd0069d8e336532949b4e35ba29f93e7a
yousef fathy <yf1102003@gmail.com>
2023-05-19 18:11:44 +0300

    Accepted!!!

--- ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
+++ ICPC-Assiut-University-Community/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     double r;
     const double pi=3.141592653;
-    cin>>r;
-    cout<<pi*r*r<<fixed<<setprecision(9)<<endl;
+    cin >> r;
+    cout << fixed << setprecision(9) << pi*r*r <<endl;
     return 0;
 }
