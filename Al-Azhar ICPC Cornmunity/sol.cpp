562430647fcfa7f156ef4319c26b3577ca9cc94d
yousef fathy <yf1102003@gmail.com>
2023-08-07 04:57:40 +0300

    updating

--- Al-Azhar ICPC Cornmunity/sol.cpp
+++ Al-Azhar ICPC Cornmunity/sol.cpp
@@ -6,8 +6,13 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    long x, y;
-    cin >> x >> y;
-    string result = (x % y == 0 || y % x == 0) ? "Multiples" : "No Multiples";
-    cout << result;
+    double x;
+    cin >> x;
+    if (x == int(x))
+    {
+       cout << "int " << int(x);
+    }else
+    {
+        cout << "float " << x - int(x);
+    }
 }
