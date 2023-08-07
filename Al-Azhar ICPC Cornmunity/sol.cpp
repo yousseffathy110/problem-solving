c12ccdc54aad30fdafcc8a3ff4ad9962906e685c
yousef fathy <yf1102003@gmail.com>
2023-08-07 03:23:27 +0300

    updating

--- Al-Azhar ICPC Cornmunity/sol.cpp
+++ Al-Azhar ICPC Cornmunity/sol.cpp
@@ -6,5 +6,8 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+    long x, y;
+    cin >> x >> y;
+    string result = (x % y == 0 || y % x == 0) ? "Multiples" : "No Multiples";
+    cout << result;
 }
