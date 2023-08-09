9f9f5dd22efa228e4af37c55ab5b993137e5653b
yousef fathy <yf1102003@gmail.com>
2023-08-09 21:18:47 +0300

    Accepted!!!

--- ProblemSet/A. Way Too Long Words.cpp
+++ ProblemSet/A. Way Too Long Words.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main()
 {
+    short input;
+    cin >> input;
+    while (input > 0)
+    {
+        string word;
+        cin >> word;
+        int size = word.length()-1;
+        if(word.length() <= 10){
+            cout << word << endl;
+        }
+        else
+        {
+            int count = 0;
+            for(int i = 1; i < word.length()-1 ; i++){
+                count += 1;
+            }
+            cout << word[0] << count << word[size] << endl;
+        }
+        input --;
+    }
     
 }
