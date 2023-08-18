6cf31f0f01602f42f676787d19e57a87e606e6d6
yousef fathy <yf1102003@gmail.com>
2023-08-18 14:26:48 +0300

    Still

--- ProblemSet/A. Boy or Girl.cpp
+++ ProblemSet/A. Boy or Girl.cpp
@@ -1,14 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fast(){
+void fast() {
     cin.sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 }
 
-
-int main()
-{
+int main() {
     fast();
+    string input ;
+    cin >> input;
+    vector <char> name;
+    for(char letter : input)
+        name.push_back(letter);
+    short counter = 0;
+    for (int i = 0; i < name.size(); i++) {
+        for (int j = i + 1; j < name.size(); j++) {
+            if (name[i] == name[j]) {
+                counter++;
+                // name.erase(name[j]);
+            }
+        }
+    }
+    if ((name.size() - counter) % 2 == 0)
+        cout << "CHAT WITH HER!";
+    else
+        cout << "IGNORE HIM!";
+    return 0;
 }
