5b35f51845aac7c5687a27025c5b00915e70a8a9
yousef fathy <yf1102003@gmail.com>
2023-08-18 13:30:26 +0300

    Accepted!!!

--- ProblemSet/A. Helpful Maths.cpp
+++ ProblemSet/A. Helpful Maths.cpp
@@ -7,8 +7,22 @@ void fast(){
     cout.tie(0);
 }
 
+
 int main()
 {
     fast();
-     
+    string input;
+    cin >> input;
+    if (input.length() == 1)
+        cout << input;
+    else
+    {
+        for(int i = 0 ; i < input.length() - 1 ; i++){
+            for(int j = 0 ; j < input.length() - i - 1 ; j += 2){
+                if (input[j] >= input[j + 2])
+                    swap(input[j], input[j +2]);
+            }
+        }
+        cout << input;
+    }
 }
