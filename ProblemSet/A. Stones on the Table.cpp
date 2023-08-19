b46fce0aab58ceb260542b617eb6499cacf23421
yousef fathy <yf1102003@gmail.com>
2023-08-19 16:16:49 +0300

    Accepted!!!

---
+++ ProblemSet/A. Stones on the Table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void fast() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast();
+    
+    return 0;
+}
