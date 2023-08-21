e2bd1860556272304b98522941f3d0dda308c186
yousef fathy <yf1102003@gmail.com>
2023-08-21 16:42:11 +0300

    Accepted!!!

--- CSES Problem Set/Introductory Problems/Missing Number.cpp
+++ CSES Problem Set/Introductory Problems/Missing Number.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-
-int main(){
-    
+int main()
+{
+    ll n , sum = 0;
+    cin >> n;
+    for (int i = 1; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        sum += x;
+    }
+    cout << (n*(n+1) / 2) - sum;
     return 0;
 }
--- CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
+++ CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
@@ -10,11 +10,10 @@ int main(){
     {
         if(n % 2 == 0){
             n /=2 ;
-            cout << n << " ";
         }else{
             n = 3*n +1;
-            cout << n << " ";
         }
+        cout << n << " ";
     }
     return 0;
 }
